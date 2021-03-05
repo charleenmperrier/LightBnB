57057e12a7d24f374799f334b9363e96b313ccde
charleenmperrier <charlee_perrier@hotmail.com>
2021-03-05 03:21:28 +0000

    add property func

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -181,9 +181,34 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(`
+  INSERT INTO properties (title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code) 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *
+  ;
+  `, [property.title, property.description, property.owner_id, property.cover_photo_url, property.thumbnail_photo_url, (property.cost_per_night)*100, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.province, property.city, property.country, property.street, property.post_code])
+  .then(res => { 
+    return res.rows;
+  });
 }
 exports.addProperty = addProperty;
+
+
+// INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
+// VALUES (1, 1, '2018-09-11', '2018-09-26'),
+// {
+//   owner_id: int,
+//   title: string,
+//   description: string,
+//   thumbnail_photo_url: string,
+//   cover_photo_url: string,
+//   cost_per_night: string,
+//   street: string,
+//   city: string,
+//   province: string,
+//   post_code: string,
+//   country: string,
+//   parking_spaces: int,
+//   number_of_bathrooms: int,
+//   number_of_bedrooms: int
+// }
